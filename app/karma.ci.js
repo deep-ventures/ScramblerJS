38d2e759e83df545b4e46935dcdf7f3cef4faf58
Patrick Arminio <patrick.arminio@gmail.com>
2015-12-31 15:06:25 +0100

    Added saucelabs

--- app/karma.ci.js
+++ app/karma.ci.js
@@ -2,73 +2,92 @@
 // Generated on Tue Dec 29 2015 23:03:17 GMT+0100 (CET)
 
 module.exports = function(config) {
-  config.set({
+    var customLaunchers = {
+        sl_chrome: {
+            base: 'SauceLabs',
+            browserName: 'chrome',
+            platform: 'Windows 7',
+            version: '35'
+        },
+        sl_firefox: {
+            base: 'SauceLabs',
+            browserName: 'firefox',
+            version: '30'
+        },
+        sl_ios_safari: {
+            base: 'SauceLabs',
+            browserName: 'iphone',
+            platform: 'OS X 10.9',
+            version: '7.1'
+        },
+        sl_ie_11: {
+            base: 'SauceLabs',
+            browserName: 'internet explorer',
+            platform: 'Windows 8.1',
+            version: '11'
+        }
+    }
 
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
+    config.set({
 
-
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine', 'fixture'],
-
-
-    // list of files / patterns to load in the browser
-    files: [
-      'src/*.js',
-      'tests/*.js',
-      'fixtures/*.html'
-    ],
-
-
-    // list of files to exclude
-    exclude: [
-    ],
-
-
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-        'src/scrambler.js': ['coverage'],
-        'fixtures/*.html': ['html2js']
-    },
-
-    coverageReporter: {
-        type: 'lcov',
-        dir: 'coverage/'
-    },
-
-    reporters: ['progress', 'dots', 'coverage', 'coveralls'],
+        // base path that will be used to resolve all patterns (eg. files, exclude)
+        basePath: '',
 
 
-    // web server port
-    port: 9876,
+        // frameworks to use
+        // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+        frameworks: ['jasmine', 'fixture'],
 
 
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
+        // list of files / patterns to load in the browser
+        files: [
+            'src/*.js',
+            'tests/*.js',
+            'fixtures/*.html'
+        ],
 
 
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
+        // list of files to exclude
+        exclude: [
+        ],
 
 
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
+        // preprocess matching files before serving them to the browser
+        // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+        preprocessors: {
+            'fixtures/*.html': ['html2js']
+        },
+
+        coverageReporter: {
+            type: 'lcov',
+            dir: 'coverage/'
+        },
+
+        reporters: ['dots', 'saucelabs'],
 
 
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome', 'Firefox', 'Safari'],
+        // web server port
+        port: 9876,
 
 
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: true,
+        // enable / disable colors in the output (reporters and logs)
+        colors: true,
 
-    // Concurrency level
-    // how many browser should be started simultaneous
-    concurrency: Infinity
-  })
+
+        // level of logging
+        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+        logLevel: config.LOG_DEBUG,
+
+
+        sauceLabs: {
+            testName: 'ScramblerJS Unit Tests',
+        },
+
+        customLaunchers: customLaunchers,
+        browsers: Object.keys(customLaunchers),
+
+        // Continuous Integration mode
+        // if true, Karma captures browsers, runs the tests and exits
+        singleRun: true
+    })
 }
