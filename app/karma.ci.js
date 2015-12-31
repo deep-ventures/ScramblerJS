1306ddbea70e2cc93fb487dc18772d4540470c20
Patrick Arminio <patrick.arminio@gmail.com>
2016-01-01 00:39:53 +0100

    Max 3 browser at the same time

--- app/karma.ci.js
+++ app/karma.ci.js
@@ -111,6 +111,7 @@ module.exports = function(config) {
 
         // Continuous Integration mode
         // if true, Karma captures browsers, runs the tests and exits
-        singleRun: true
+        singleRun: true,
+        concurrency: 3
     })
 }
