be44bc1db25bc49eb30ba22657fad10aba8fa45e
grudelsud <thomasalisi@gmail.com>
2015-12-31 13:07:48 +0100

    removed unused lab files and implement dialog, wip but almost there

--- app/gulpfile.js
+++ app/gulpfile.js
@@ -2,12 +2,21 @@ var gulp = require("gulp");
 var concat = require("gulp-concat");
 
 var paths = {
+    assets: [
+        "assets/**/*"
+    ],
     src: [
         "node_modules/alertify.js/dist/js/alertify.js",
         "src/**/*.js"
     ],
     out: "build"
 };
+
+gulp.task("assets", function() {
+    return gulp
+        .src(paths.assets)
+        .pipe(gulp.dest(paths.out + "/assets"));
+});
 
 gulp.task("build", function() {
     return gulp
@@ -17,7 +26,8 @@ gulp.task("build", function() {
 });
 
 gulp.task("watch", function() {
+    gulp.watch(paths.assets, ["assets"]);
     gulp.watch(paths.src, ["build"]);
 });
 
-gulp.task("default", ["watch"]);
+gulp.task("default", ["assets", "build", "watch"]);
