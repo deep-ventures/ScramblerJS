609d4cd21d00ebb81a6d393253246b441b755510
Patrick Arminio <patrick.arminio@gmail.com>
2015-12-31 15:42:52 +0100

    Fix issue on Internet Explorer 11

--- app/src/scrambler.js
+++ app/src/scrambler.js
@@ -7,7 +7,9 @@
             a = [],
             walk = document.createTreeWalker(
                 el,
-                NodeFilter.SHOW_TEXT);
+                NodeFilter.SHOW_TEXT,
+                null,
+                false);
 
         while (n = walk.nextNode())
             a.push(n);
