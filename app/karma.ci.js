fa6f63cc79ab74193f1e5e27d5d48335872090eb
Patrick Arminio <patrick.arminio@gmail.com>
2016-01-09 16:06:34 +0100

    Fix IE version

--- app/karma.ci.js
+++ app/karma.ci.js
@@ -37,7 +37,7 @@ module.exports = function(config) {
         sl_ie_10: {
             base: 'SauceLabs',
             browserName: 'internet explorer',
-            version: '9'
+            version: '10'
         },
         sl_ie_9: {
             base: 'SauceLabs',
